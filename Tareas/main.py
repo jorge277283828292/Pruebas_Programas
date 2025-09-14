c02b23392cef5f611321b153ab9b01029f811fc2
Jorge Villarreal <jv7484031@gmail.com>
2025-09-14 14:38:59 -0600

    Cambios en apariencia del programa.

--- Tareas/main.py
+++ Tareas/main.py
@@ -5,7 +5,7 @@ from ver import ver_tareas
 from completadas import tareas_completadas
 from restaurar import restaurar_tareas
 from listas import Tareas, Tareas_completadas, Tareas_eliminadas
-
+import time
 
 while True:
     opcioness()
@@ -13,26 +13,32 @@ while True:
     if opcion == "1":
     #Agregar
         agregar_tareas()
+        time.sleep(1.5)
         continue
     #Eliminar
     elif opcion == "2":
         eliminar_tareas()
+        time.sleep(1.5)
         continue
     #Ver tareas
     elif opcion == "3":
         ver_tareas()
+        time.sleep(1.5)
         continue
     #Marcar como completada
     elif opcion == "4":
         tareas_completadas()
+        time.sleep(1.5)
         continue
     elif opcion == "5":
         restaurar_tareas()
+        time.sleep(1.5)
         continue
     #Salir
     elif opcion == "6":
         print("Saliendo del programa...")
         break
+    #Error
     else:
         print("Opcion no valida, intenta de nuevo")
         continue
